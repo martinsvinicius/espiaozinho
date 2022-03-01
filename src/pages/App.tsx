c40a5ff44615ad0507184587d9e03960237225b7
martinsvinicius <vinicius.victor.sm@gmail.com>
2022-03-01 03:37:40 -0300

    Project Structure 🚧

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import { Flex, Text } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Flex w="400px" mx="auto" mt={4} align="center" flexDir="column">
+      <Text fontSize={32} color="white">
+        spyfall
+      </Text>
+      <Text color="purple.500" mt={2}>
+        coming soon...
+      </Text>
+    </Flex>
+  )
+}
+
+export default App
---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    black: {
+      300: '#01161E',
+    },
+    purple: {
+      500: '#7067CF',
+      50: '#B7C0EE',
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bgColor: 'black.300',
+      },
+    },
+  },
+})
