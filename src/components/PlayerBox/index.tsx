4ea2b78c48d3cb15939a195207c5e9d5708954be
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-03-01 20:49:09 -0300

    PlayerBox component created

---
+++ src/components/PlayerBox/index.tsx
@@ -0,0 +1,51 @@
+import { Flex, Image, Text } from '@chakra-ui/react'
+
+interface PlayerBoxProps {
+  name: string
+  color: string
+}
+
+export function PlayerBox({ name, color }: PlayerBoxProps) {
+  const red =
+    'invert(37%) sepia(98%) saturate(7489%) hue-rotate(356deg) brightness(107%) contrast(123%)'
+  const blue =
+    'invert(8%) sepia(100%) saturate(6400%) hue-rotate(246deg) brightness(107%) contrast(147%)'
+  const yellow =
+    'invert(98%) sepia(68%) saturate(2872%) hue-rotate(356deg) brightness(105%) contrast(102%)'
+
+  const imageColor = () => {
+    switch (color) {
+      case 'red':
+        return red
+      case 'blue':
+        return blue
+      case 'yellow':
+        return yellow
+      default:
+        return 'red'
+    }
+  }
+
+  const removePlayer = () => {
+    console.log('removendo jogador')
+  }
+
+  return (
+    <Flex w="100%" justify="space-between" px={10} py={3}>
+      <Flex>
+        <Image src="assets/icons/spy.svg" alt="Spy" filter={imageColor()} />
+        <Text color="white" fontSize={18} fontWeight="medium" ml={4}>
+          {name}
+        </Text>
+      </Flex>
+      <Image
+        onClick={removePlayer}
+        src="assets/icons/remove.svg"
+        alt="Remove"
+        _hover={{
+          cursor: 'pointer',
+        }}
+      />
+    </Flex>
+  )
+}
