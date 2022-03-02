475e0f8c6b870103bdd03915dce71874fac08a96
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2022-03-02 01:05:06 -0300

    routing created

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './pages/App'
+import Game from './pages/Game'
 import { theme } from './theme'
 import { ContextProvider } from './contexts/PlayersContext'
 
@@ -10,7 +12,12 @@ ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
       <ChakraProvider theme={theme}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="game" element={<Game />} />
+          </Routes>
+        </BrowserRouter>
       </ChakraProvider>
     </ContextProvider>
   </React.StrictMode>,
